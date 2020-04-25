53c9300a9d6e9d0626f5b2f94ebcca0b3463e02f
VoyagerWD <56484622+VoyagerWD@users.noreply.github.com>
2020-04-25 20:52:31 -0300

    Sec
    Making safer

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 class Database{
 	public static function connect(){
-		$db = new mysqli('localhost', 'root', '', 'donlechugon');
+		$db = new mysqli('****', '*****', '', '*****);
 		$db->query("SET NAMES 'utf8'");
 		return $db;
 	}
